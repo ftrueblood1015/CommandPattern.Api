7b25eeaa0dfabc8dd276152f1882e29cb557b31f
Fletcher Trueblood <ftrueblood@jgboswell.com>
2024-01-12 11:30:04 -0800

    Refactor deck card grid into separate component

--- CommandPattern.BlazorServer/Pages/Mtg/Decks/DeckDetail.razor.cs
+++ CommandPattern.BlazorServer/Pages/Mtg/Decks/DeckDetail.razor.cs
@@ -8,19 +8,10 @@ namespace CommandPattern.BlazorServer.Pages.Mtg.Decks
 {
     public partial class DeckDetail : DetailPageBase<Deck>
     {
-        [Inject]
-        private IDeckCardService? DeckCardService { get; set; }
-
         [Parameter]
-        public int EntityId { get; set; }
+        public long EntityId { get; set; }
 
         public Deck? Entity { get; set; } = new Deck();
-
-        private List<DeckCard>? DeckCards { get; set; }
-
-        public MudForm? DeckCardForm;
-
-        public DeckCard? DeckCard { get; set; } = new DeckCard();
 
         protected override async Task OnInitializedAsync()
         {
@@ -35,25 +26,8 @@ namespace CommandPattern.BlazorServer.Pages.Mtg.Decks
             }
             else
             {
-                var result = await base.GetEntity(EntityId);
-                if (result != null)
-                {
-                    await GetDeckCards();
-                }
-
-                return result;
+                return await base.GetEntity((int)EntityId);
             }
-        }
-
-        private async Task GetDeckCards()
-        {
-            if ( DeckCardService == null)
-            {
-                throw new ArgumentNullException(nameof(DeckCardService));
-            }
-
-            var Response = await DeckCardService.GetAllEntitiesFiltered(EntityId);
-            DeckCards = Response != null ? Response.ToList() : new List<DeckCard>();
         }
 
         public async Task ThemeChange(long? Id)
@@ -69,69 +43,6 @@ namespace CommandPattern.BlazorServer.Pages.Mtg.Decks
         public async Task GuildChange(long? Id)
         {
             Entity!.GuildId = (long)Id!;
-        }
-
-        public async Task DeckCardChange(long? Id)
-        {
-            DeckCard!.CardId = (long)Id!;
-        }
-
-        private async Task<bool> RemoveDeckCard(DeckCard Item)
-        {
-            if (DeckCardService == null)
-            {
-                throw new Exception($"{nameof(DeckCardService)} is null!");
-            }
-
-            try
-            {
-                var result = await DeckCardService.DeleteEntity(Item.Id);
-
-                if (result)
-                {
-                    ShowSnackbarMessage($"Deleted {Item.Name}", MudBlazor.Color.Success);
-                    await GetDeckCards();
-                }
-                else
-                {
-                    ShowSnackbarMessage($"Could Not Delete {Item.Name}", MudBlazor.Color.Error);
-                }
-
-                return result;
-            }
-            catch (Exception ex)
-            {
-                ShowSnackbarMessage($"Could Not Delete {Item.Name}: {ex}", MudBlazor.Color.Error);
-                return false;
-            }
-        }
-
-        private async void SaveDeckCard(DeckCard Entity)
-        {
-            try
-            {
-                // add what is needed to deck card and create it
-                Entity.IsActive = true;
-                Entity.Name = "Card";
-                Entity.DeckId = EntityId;
-                await DeckCardService!.InsertEntity(Entity);
-
-                success = true;
-                ShowSnackbarMessage($"Added New {Entity.Name}", MudBlazor.Color.Success);
-
-                // reset form and deck card
-                await DeckCardForm!.ResetAsync();
-                DeckCard = new DeckCard() { CardId = 0 };
-
-                // refresh deck card data grid
-                await GetDeckCards();
-                StateHasChanged();
-            }
-            catch
-            {
-                success = false;
-                ShowSnackbarMessage($"Could Not Add {Entity.Name}", MudBlazor.Color.Error);
-            }
         }
     }
 }
